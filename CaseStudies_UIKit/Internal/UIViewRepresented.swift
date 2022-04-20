7faa862cc458c09d8812177f5a398ac953c40a6d
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-04-21 08:34:45 +0900

    Update UIViewRepresented

--- CaseStudies_UIKit/Internal/UIViewRepresented.swift
+++ CaseStudies_UIKit/Internal/UIViewRepresented.swift
@@ -5,4 +5,17 @@
 //  Created by Taisei Sakamoto on 2022/04/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct UIViewRepresented<UIViewType>: UIViewRepresentable where UIViewType: UIView {
+    let makeUIView: (Context) -> UIViewType
+    let updateUIView: (UIViewType, Context) -> Void = { _, _ in }
+    
+    func makeUIView(context: Context) -> UIViewType {
+        makeUIView(context: context)
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        updateUIView(uiView, context: context)
+    }    
+}
